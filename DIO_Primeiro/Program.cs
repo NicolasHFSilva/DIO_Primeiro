9be3b85af794dfe84078e6d8247b4003f375bd7f
NicolasHFSilva <henrinicky@gmail.com>
2022-01-14 15:58:50 -0300

    Adicionar arquivos de projeto.

---
+++ DIO_Primeiro/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var a = "Hello, "; //Aqui estamos dizendo que iremos criar uma variável 'a' recebendo uma string
+var b = "World";   //Idem anterior.
+Console.WriteLine(a + b); //Teremos um OUTPUT em tela das variáveis 'a' e 'b'.
+Console.WriteLine("Hello, World!"); //Teremos um OUTPUT de uma string escrita diretamente.
